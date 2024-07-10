62340846fbd91c13c802ce4f5b6cf47822fcc49d
soapmactavish23 <ricknogueira1231@gmail.com>
2024-07-10 20:02:32 -0300

    26. Criando os producers nas aplicações

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/producer/SagaProducer.java
@@ -0,0 +1,29 @@
+package br.com.microservices.orchestrated.orderservice.core.producer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SagaProducer {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	
+	@Value("${spring.kafka.start-saga}")
+	private String startSagaTopic;
+	
+	public void sendEvent(String payload) {
+		try {
+			log.info("Sending event to topic {} with data {}", startSagaTopic, payload);
+			kafkaTemplate.send(startSagaTopic, payload);			
+		} catch (Exception e) {
+			log.error("Error trying to send data to topic {} with data {}", startSagaTopic, payload, e);
+		}
+	}
+	
+}
